e5ceb6d2b24b559865516f8d7b73e59c380d1f56
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-07-23 00:04:24 +0700

    hotfix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(200);
+        Schema::defaultStringLength(191);
     }
 }
